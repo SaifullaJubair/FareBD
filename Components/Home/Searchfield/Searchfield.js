8511f97ec9a0f33e5c6bb9d54da171c1e87e92a6
anik-kumaar-datta <anikkumaardatta@gmail.com>
2023-02-19 03:55:50 +0600

    add react hook form

--- Components/Home/Searchfield/Searchfield.js
+++ Components/Home/Searchfield/Searchfield.js
@@ -1,9 +1,31 @@
+
+import React from "react";
+import { useForm } from 'react-hook-form';
 // Anik Datta 
 
 const Searchfield = () => {
+
+   // implement search field using react-hook-form
+
+   const { register, handleSubmit, formState: { errors } } = useForm();
+   const onSubmit = data => console.log(data);
+   console.log(errors);
+
+
    return (
-      <div>
-         <h2>This is search Field</h2>
+      <div className="max-w-[1440px] w-[95%] mx-auto">
+         <form onSubmit={handleSubmit(onSubmit)}>
+            <input type="text" placeholder="Location (city, state, area)"
+               {...register("location", {required: true})}
+            />
+            <select {...register("Title", { required: true })}>
+               <option value="apartment">Apartment</option>
+               <option value="duplex">Duplex</option>
+               <option value="penthouse">Penthouse</option>
+               <option value="office">Office</option>
+            </select>
+            <input type="submit" />
+         </form>
       </div>
    );
 };
