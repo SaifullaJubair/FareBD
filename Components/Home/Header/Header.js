ae09053a562ac2bedcf59ea61d83a175bbcf0d34
Rezaul <mdrezaulkarrim@gmail.com>
2023-02-19 01:45:15 +0600

    added swiper package

--- Components/Home/Header/Header.js
+++ Components/Home/Header/Header.js
@@ -1,12 +1,24 @@
+import { Swiper, SwiperSlide } from "swiper/react";
 
-
+// Import Swiper styles
+import "swiper/css";
 const Header = () => {
-   return (
-      <div>
-         <h1>This is header</h1>
-
-      </div>
-   );
+  return (
+    <div>
+      <h1>This is header Rezaul</h1>
+      <Swiper
+        spaceBetween={50}
+        slidesPerView={3}
+        onSlideChange={() => console.log("slide change")}
+        onSwiper={(swiper) => console.log(swiper)}
+      >
+        <SwiperSlide>Slide 1</SwiperSlide>
+        <SwiperSlide>Slide 2</SwiperSlide>
+        <SwiperSlide>Slide 3</SwiperSlide>
+        <SwiperSlide>Slide 4</SwiperSlide>
+      </Swiper>
+    </div>
+  );
 };
 
 export default Header;
