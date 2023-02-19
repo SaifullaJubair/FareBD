b79e5a035965d32191570fc2b2cb9b25c176a388
zahidhasan125 <zahidhasan125@gmail.com>
2023-02-19 17:55:51 +0600

    installed react-icons, card responsive

--- Components/Home/About/About.js
+++ Components/Home/About/About.js
@@ -1,10 +1,87 @@
-
+import { MdOutlineRealEstateAgent, MdSupportAgent } from 'react-icons/md';
+import { GrMapLocation } from 'react-icons/gr';
+import { AiFillFormatPainter } from 'react-icons/ai';
+import { GoChecklist } from 'react-icons/go';
+import { GiReceiveMoney } from 'react-icons/gi';
 
 const About = () => {
    return (
-      <div>
-         <h2>This is about</h2>
-      </div>
+      <section className="max-w-7xl w-full bg-[#F5F8FA] mx-auto py-24 px-4">
+         <div className="flex flex-col items-center justify-center max-w-4xl mx-auto">
+            <h3 className="text-xl">WHY CHOOSE US</h3>
+            <h4 className="text-xl md:text-2xl font-bold my-6 text-center">6 Reasons to Choose FareBD</h4>
+            <p className="text-center">Annually, thousands of clients choose Estancy as their real estate agency. Besides having years of experience in real estate industry, there is a variety of reasons why residents over the world choose us. Here are just some of them.</p>
+         </div>
+         <div className="grid grid-cols-1 md:grid-cols-3">
+
+            <div class="max-w-sm p-6 dark:bg-gray-800 dark:border-gray-700 flex flex-col items-center justify-center">
+               <div className="p-4">
+                  <div className='bg-secondary w-12 h-12 absolute rounded-full'></div>
+                  <MdOutlineRealEstateAgent className='text-5xl relative left-6 font-thin' />
+               </div>
+               <a href="#">
+                  <h5 class="mb-2 text-2xl font-semibold tracking-tight text-gray-900 dark:text-white">Professional & Friendly Agents</h5>
+               </a>
+               <p class="mb-3 font-normal text-gray-500 dark:text-gray-400 text-center">Our agents are experienced and understanding experts who deliver fresh and effective solutions.</p>
+            </div>
+
+            <div class="max-w-sm p-6 dark:bg-gray-800 dark:border-gray-700 flex flex-col items-center justify-center">
+               <div className="p-4">
+                  <div className='bg-secondary w-12 h-12 absolute rounded-full'></div>
+                  <GrMapLocation className='text-5xl relative left-6 font-thin' />
+               </div>
+               <a href="#">
+                  <h5 class="mb-2 text-2xl font-semibold tracking-tight text-gray-900 dark:text-white">Need a help in Claim?</h5>
+               </a>
+               <p class="mb-3 font-normal text-gray-500 dark:text-gray-400 text-center">Go to this step by step guideline process on how to certify for your weekly benefits:</p>
+            </div>
+
+            <div class="max-w-sm p-6 dark:bg-gray-800 dark:border-gray-700 flex flex-col items-center justify-center">
+               <div className="p-4">
+                  <div className='bg-secondary w-12 h-12 absolute rounded-full'></div>
+                  <AiFillFormatPainter className='text-5xl relative left-6 font-thin' />
+               </div>
+               <a href="#">
+                  <h5 class="mb-2 text-2xl font-semibold tracking-tight text-gray-900 dark:text-white">Need a help in Claim?</h5>
+               </a>
+               <p class="mb-3 font-normal text-gray-500 dark:text-gray-400 text-center">Go to this step by step guideline process on how to certify for your weekly benefits:</p>
+            </div>
+
+            <div class="max-w-sm p-6 dark:bg-gray-800 dark:border-gray-700 flex flex-col items-center justify-center">
+               <div className="p-4">
+                  <div className='bg-secondary w-12 h-12 absolute rounded-full'></div>
+                  <MdSupportAgent className='text-5xl relative left-6 font-thin' />
+               </div>
+               <a href="#">
+                  <h5 class="mb-2 text-2xl font-semibold tracking-tight text-gray-900 dark:text-white">Need a help in Claim?</h5>
+               </a>
+               <p class="mb-3 font-normal text-gray-500 dark:text-gray-400 text-center">Go to this step by step guideline process on how to certify for your weekly benefits:</p>
+            </div>
+
+            <div class="max-w-sm p-6 dark:bg-gray-800 dark:border-gray-700 flex flex-col items-center justify-center">
+               <div className="p-4">
+                  <div className='bg-secondary w-12 h-12 absolute rounded-full'></div>
+                  <GoChecklist className='text-5xl relative left-6 font-thin' />
+               </div>
+               <a href="#">
+                  <h5 class="mb-2 text-2xl font-semibold tracking-tight text-gray-900 dark:text-white">Need a help in Claim?</h5>
+               </a>
+               <p class="mb-3 font-normal text-gray-500 dark:text-gray-400 text-center">Go to this step by step guideline process on how to certify for your weekly benefits:</p>
+            </div>
+
+            <div class="max-w-sm p-6 dark:bg-gray-800 dark:border-gray-700 flex flex-col items-center justify-center">
+               <div className="p-4">
+                  <div className='bg-secondary w-12 h-12 absolute rounded-full'></div>
+                  <GiReceiveMoney className='text-5xl relative left-6 font-thin' />
+               </div>
+               <a href="#">
+                  <h5 class="mb-2 text-2xl font-semibold tracking-tight text-gray-900 dark:text-white">Need a help in Claim?</h5>
+               </a>
+               <p class="mb-3 font-normal text-gray-500 dark:text-gray-400 text-center">Go to this step by step guideline process on how to certify for your weekly benefits:</p>
+            </div>
+
+         </div>
+      </section>
    );
 };
 
--- Components/Shared/Footer/Footer.js
+++ Components/Shared/Footer/Footer.js
@@ -5,8 +5,7 @@ const Footer = () => {
       <footer className="bg-primary text-white">
          <div className="max-w-[1440px] w-[95%] mx-auto">
             <h2 className="text-4xl font-bold">This is footer</h2>
-            <p className="text-white">This is for test purpose.</p>
-            <textarea name="" id="" cols="30" rows="10"></textarea>
+            <p className="text-white bg-secondary">This is for test purpose.</p>
          </div>
       </footer>
    );
