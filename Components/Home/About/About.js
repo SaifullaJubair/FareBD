a04847ad146f67359555361badc79468a195cb99
zahidhasan125 <zahidhasan125@gmail.com>
2023-02-19 22:58:37 +0600

    footer added

--- Components/Home/About/About.js
+++ Components/Home/About/About.js
@@ -16,70 +16,70 @@ const About = () => {
             </div>
             <div className="grid grid-cols-1 md:grid-cols-3">
                {/* About cards */}
-               <div class="max-w-sm dark:bg-gray-800 dark:border-gray-700 flex flex-col items-center justify-center mt-8">
+               <div className="max-w-sm dark:bg-gray-800 dark:border-gray-700 flex flex-col items-center justify-center mt-8">
                   <div className="p-4">
                      <div className='bg-secondary w-12 h-12 absolute rounded-full'></div>
                      <MdOutlineRealEstateAgent className='text-5xl relative left-6 font-thin' />
                   </div>
                   <a href="#">
-                     <h5 class="mb-2 text-xl text-center font-semibold tracking-tight text-gray-900 dark:text-white">Professional & Friendly Agents</h5>
+                     <h5 className="mb-2 text-xl text-center font-semibold tracking-tight text-gray-900 dark:text-white">Professional & Friendly Agents</h5>
                   </a>
-                  <p class="mb-3 font-normal text-gray-500 dark:text-gray-400 text-center">Our agents are experienced and understanding experts who deliver fresh and effective solutions.</p>
+                  <p className="mb-3 font-normal text-gray-500 dark:text-gray-400 text-center">Our agents are experienced and understanding experts who deliver fresh and effective solutions.</p>
                </div>
 
-               <div class="max-w-sm dark:bg-gray-800 dark:border-gray-700 flex flex-col items-center justify-center mt-8">
+               <div className="max-w-sm dark:bg-gray-800 dark:border-gray-700 flex flex-col items-center justify-center mt-8">
                   <div className="p-4">
                      <div className='bg-secondary w-12 h-12 absolute rounded-full'></div>
                      <GrMapLocation className='text-5xl relative left-6 font-thin' />
                   </div>
                   <a href="#">
-                     <h5 class="mb-2 text-xl text-center font-semibold tracking-tight text-gray-900 dark:text-white">Property from Anywhere</h5>
+                     <h5 className="mb-2 text-xl text-center font-semibold tracking-tight text-gray-900 dark:text-white">Property from Anywhere</h5>
                   </a>
-                  <p class="mb-3 font-normal text-gray-500 dark:text-gray-400 text-center">With FareBD, you can search for a desired property from any location.</p>
+                  <p className="mb-3 font-normal text-gray-500 dark:text-gray-400 text-center">With FareBD, you can search for a desired property from any location.</p>
                </div>
 
-               <div class="max-w-sm dark:bg-gray-800 dark:border-gray-700 flex flex-col items-center justify-center mt-8">
+               <div className="max-w-sm dark:bg-gray-800 dark:border-gray-700 flex flex-col items-center justify-center mt-8">
                   <div className="p-4">
                      <div className='bg-secondary w-12 h-12 absolute rounded-full'></div>
                      <AiFillFormatPainter className='text-5xl relative left-6 font-thin' />
                   </div>
                   <a href="#">
-                     <h5 class="mb-2 text-xl text-center font-semibold tracking-tight text-gray-900 dark:text-white">Well-furnished Interiors</h5>
+                     <h5 className="mb-2 text-xl text-center font-semibold tracking-tight text-gray-900 dark:text-white">Well-furnished Interiors</h5>
                   </a>
-                  <p class="mb-3 font-normal text-gray-500 dark:text-gray-400 text-center">All our properties are furnished and decorated to meet your needs and expectations.</p>
+                  <p className="mb-3 font-normal text-gray-500 dark:text-gray-400 text-center">All our properties are furnished and decorated to meet your needs and expectations.</p>
                </div>
 
-               <div class="max-w-sm dark:bg-gray-800 dark:border-gray-700 flex flex-col items-center justify-center mt-8">
+               <div className="max-w-sm dark:bg-gray-800 dark:border-gray-700 flex flex-col items-center justify-center mt-8">
                   <div className="p-4">
                      <div className='bg-secondary w-12 h-12 absolute rounded-full'></div>
                      <MdSupportAgent className='text-5xl relative left-6 font-thin' />
                   </div>
                   <a href="#">
-                     <h5 class="mb-2 text-xl text-center font-semibold tracking-tight text-gray-900 dark:text-white">Get support when you need it</h5>
+                     <h5 className="mb-2 text-xl text-center font-semibold tracking-tight text-gray-900 dark:text-white">Get support when you need it</h5>
                   </a>
-                  <p class="mb-3 font-normal text-gray-500 dark:text-gray-400 text-center">FareBD's support experts are always ready to help you and answer your questions.</p>
+                  <p className="mb-3 font-normal text-gray-500 dark:text-gray-400 text-center">FareBD's support experts are always ready to help you and answer your questions.</p>
                </div>
 
-               <div class="max-w-sm dark:bg-gray-800 dark:border-gray-700 flex flex-col items-center justify-center mt-8">
+               <div className="max-w-sm dark:bg-gray-800 dark:border-gray-700 flex flex-col items-center justify-center mt-8">
                   <div className="p-4">
                      <div className='bg-secondary w-12 h-12 absolute rounded-full'></div>
                      <GoChecklist className='text-5xl relative left-6 font-thin' />
                   </div>
                   <a href="#">
-                     <h5 class="mb-2 text-xl text-center font-semibold tracking-tight text-gray-900 dark:text-white">Checked & Pre-inspected Properties</h5>
+                     <h5 className="mb-2 text-xl text-center font-semibold tracking-tight text-gray-900 dark:text-white">Checked & Pre-inspected Properties</h5>
                   </a>
-                  <p class="mb-3 font-normal text-gray-500 dark:text-gray-400 text-center">All our properties are inspected to make sure they meet all standards and state regulations.</p>
+                  <p className="mb-3 font-normal text-gray-500 dark:text-gray-400 text-center">All our properties are inspected to make sure they meet all standards and state regulations.</p>
                </div>
 
-               <div class="max-w-sm dark:bg-gray-800 dark:border-gray-700 flex flex-col items-center justify-center mt-8">
+               <div className="max-w-sm dark:bg-gray-800 dark:border-gray-700 flex flex-col items-center justify-center mt-8">
                   <div className="p-4">
                      <div className='bg-secondary w-12 h-12 absolute rounded-full'></div>
                      <GiReceiveMoney className='text-5xl relative left-6 font-thin' />
                   </div>
                   <a href="#">
-                     <h5 class="mb-2 text-xl text-center font-semibold tracking-tight text-gray-900 dark:text-white">Affordable Property Pricing</h5>
+                     <h5 className="mb-2 text-xl text-center font-semibold tracking-tight text-gray-900 dark:text-white">Affordable Property Pricing</h5>
                   </a>
-                  <p class="mb-3 font-normal text-gray-500 dark:text-gray-400 text-center">We offer reasonable pricing policy on all our residential properties across the USA.</p>
+                  <p className="mb-3 font-normal text-gray-500 dark:text-gray-400 text-center">We offer reasonable pricing policy on all our residential properties across the USA.</p>
                </div>
 
             </div>
