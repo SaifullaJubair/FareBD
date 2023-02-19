3bd8e48d3d91dfc6134cf22ff01b21f2f7710626
zahidhasan125 <zahidhasan125@gmail.com>
2023-02-19 14:38:12 +0600

    Hello WORLD

--- Components/Shared/Footer/Footer.js
+++ Components/Shared/Footer/Footer.js
@@ -6,6 +6,7 @@ const Footer = () => {
          <div className="max-w-[1440px] w-[95%] mx-auto">
             <h2 className="text-4xl font-bold">This is footer</h2>
             <p className="text-white">This is for test purpose.</p>
+            <textarea name="" id="" cols="30" rows="10"></textarea>
          </div>
       </footer>
    );
