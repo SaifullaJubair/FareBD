4ab3b1c9e00da924fa3ae172c9f4d380a4392805
mostafizur610 <mostafizurrahman273@gmail.com>
2023-02-19 19:40:23 +0600

    solved config file

--- Components/Home/Review/Review.js
+++ Components/Home/Review/Review.js
@@ -4,9 +4,8 @@ import { Button } from "flowbite-react";
 const Review = () => {
    return (
       <div>
-         <h2 className="text-7xl font-bold">This is Review</h2>
+         <h2 className="text-3xl font-bold">This is Review</h2>
          <button className="bg-primary">Primary</button>
-
       </div>
    );
 };
