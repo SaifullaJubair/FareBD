66322c897df2cb875737f9e34db16b12a4070879
SaifullaJubair <saifullajubair6@gmail.com>
2023-02-20 20:24:04 +0600

    photo gallery comment

--- pages/index.js
+++ pages/index.js
@@ -9,8 +9,8 @@ import Advertise from '@/Components/Home/Advertise/Advertise'
 import About from '@/Components/Home/About/About'
 import Review from '@/Components/Home/Review/Review'
 import Branding from '@/Components/Home/Branding/Branding'
-import PhotoGallery from '@/Components/Home/PhotoGallery/PhotoGallery'
 import ContactUs from '@/Components/Home/Contactus/ContactUs'
+// import PhotoGallery from '@/Components/Home/PhotoGallery/PhotoGallery'
 import Welcome from '@/Components/Home/Welcome/Welcome'
 import Hero from '@/Components/Home/Hero/Hero'
 
@@ -29,12 +29,14 @@ export default function Home() {
       <main>
         <Hero></Hero>
         <Searchfield></Searchfield>
+
         <SellAndRent></SellAndRent>
         <Division></Division>
         <Header></Header>
         <Advertise></Advertise>
         <Welcome></Welcome>
-        <PhotoGallery></PhotoGallery>
+        {/* <PhotoGallery></PhotoGallery> */}
+
         <About></About>
         <Review></Review>
         <Branding></Branding>
