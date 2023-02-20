62beee60359c48038d65e3dd62ba2c0a817b36c1
mostafizur610 <mostafizurrahman273@gmail.com>
2023-02-20 10:12:45 +0600

    update topbar

--- Components/Shared/TopBar/TopBar.js
+++ Components/Shared/TopBar/TopBar.js
@@ -3,7 +3,7 @@ import { FaFacebook, FaGripLinesVertical, FaInstagram, FaLocationArrow, FaPhoneA
 
 const TopBar = () => {
    return (
-      <div className="bg-secondary w-full h-12 invisible lg:visible">
+      <div className="bg-secondary w-full h-12 invisible lg:visible md:visible">
          <div className='max-w-[1440px] w-[95%] mx-auto flex justify-between'>
             <div className="flex space-x-5 py-4">
                <div className='flex space-x-2'>
@@ -21,14 +21,14 @@ const TopBar = () => {
             </div>
 
             <div className="flex space-x-4 py-4">
-               <div className="text-white text-xs">Register</div>
-               <div className="text-white text-xs">Login</div>
-               <div className="text-white text-xs"><FaGripLinesVertical></FaGripLinesVertical></div>
-               <div className="text-white text-xs"><FaFacebook></FaFacebook></div>
-               <div className="text-white text-xs"><FaTwitter></FaTwitter></div>
-               <div className="text-white text-xs"><FaInstagram></FaInstagram></div>
-               <div className="text-white text-xs"><FaSkype></FaSkype></div>
-               <div className="text-white text-xs"><FaWhatsapp></FaWhatsapp></div>
+               <div className="text-white text-xs"><button>Register</button></div>
+               <div className="text-white text-xs"><button>Login</button></div>
+               <div className="text-white text-xs"><FaGripLinesVertical /></div>
+               <div className="text-white text-xs"><button><FaFacebook /></button></div>
+               <div className="text-white text-xs"><button><FaTwitter /></button></div>
+               <div className="text-white text-xs"><button><FaInstagram /></button></div>
+               <div className="text-white text-xs"><button><FaSkype /></button></div>
+               <div className="text-white text-xs"><button><FaWhatsapp /></button></div>
             </div>
          </div>
       </div>
