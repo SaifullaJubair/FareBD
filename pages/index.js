68ec9750a79ab9e5f9a1f93745e980f2566d52d8
anik-kumaar-datta <anikkumaardatta@gmail.com>
2023-02-20 18:50:25 +0600

    welcome section done

--- pages/index.js
+++ pages/index.js
@@ -1,9 +1,6 @@
 import Head from 'next/head'
 import Image from 'next/image'
 import { Inter } from '@next/font/google'
-import styles from '@/styles/Home.module.css'
-
-
 import Header from '@/Components/Home/Header/Header'
 import Searchfield from '@/Components/Home/Searchfield/Searchfield'
 import SellAndRent from '@/Components/Home/SellAndRent/SellAndRent'
