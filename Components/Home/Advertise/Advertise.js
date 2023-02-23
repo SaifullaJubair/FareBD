88a120967f8e6cf30b4de3fe7f82d9610574ecc9
amitpaultl <amitpaultl@gmail.com>
2023-02-23 12:09:27 +0600

    adviertise section dymice

--- Components/Home/Advertise/Advertise.js
+++ Components/Home/Advertise/Advertise.js
@@ -1,107 +1,77 @@
+import { useQuery } from '@tanstack/react-query';
 import home4 from "./../../../assets/images/home-4.jpg";
 import { BsPersonFill, BsArrowRight } from "react-icons/bs";
 import { BiCalendar } from "react-icons/bi";
 const Advertise = () => {
+
+  const { data: products, isLoading } = useQuery({
+    queryKey: ['user'],
+    queryFn: async () => {
+      try {
+        const res = await fetch('http://localhost:5000/property', {
+
+
+        })
+        const data = await res.json();
+        return data;
+      } catch (error) {
+        console.log(error);
+      }
+    }
+  })
+
+  // loading
+  if (isLoading) {
+    return <Loading></Loading>
+  }
+
+  const detail = 
+
+  console.log(products);
+
   return (
     <div className="mt-28 mb-16 max-w-[1440px] w-[95%] mx-auto ">
       <h2 className="text-4xl text-center font-semibold mb-10">Advertised</h2>
       <div className="flex flex-wrap items-center gap-5 justify-center">
         {/* card 1 */}
-        <div>
-          <div
-            style={{
-              backgroundImage: `linear-gradient(to bottom, rgba(0,0,0,.1), rgba(0,0,0.8)), url(${home4.src})`,
-              backgroundSize: "cover",
-            }}
-            className="w-[375px] h-80 object-cover object-center relative "
-          >
-            {/* Inner Pop Up Div */}
-          </div>
-          <div className="bg-gray-50 w-[330px] mx-auto px-5 pt-5 pb-7 transfrom -translate-y-11">
-            <div className="flex items-center gap-5 text-gray-600">
-              <div className="flex items-center gap-1">
-                <BsPersonFill className="text-xl" />
-                <span>By Admin</span>
-              </div>
-              <div className="flex items-center gap-1">
-                <BiCalendar className="text-xl" />
-                <span>March 9, 2020</span>
-              </div>
+        {
+          products?.map(advertised => <div key={advertised?._id}>
+            <div
+              style={{
+                backgroundImage: `linear-gradient(to bottom, rgba(0,0,0,.1), rgba(0,0,0.8)), url(${advertised.property_picture})`,
+                backgroundSize: "cover",
+              }}
+              className="w-[375px] h-80 object-cover object-center relative "
+            >
+              {/* Inner Pop Up Div */}
             </div>
-            <h4 className="text-2xl my-3 font-semibold hover:text-secondary transition duration-200">
-              Family House
-            </h4>
-            <p>
-              Lorem ipsum dolor sit amet consectetur adipisicing elit. Facilis
-              aspernatur eligendi sed molestias necessitatibus quia.
-            </p>
-            <BsArrowRight className="text-2xl font-semibold absolute left-1/2 transform -translate-x-1/2 -bottom-3 bg-secondary/90 transtion duration-300 cursor-pointer hover:bg-secondary w-8 h-8 rounded-full p-1 text-white" />
-          </div>
-        </div>
+            <div className="bg-gray-50 w-[330px] mx-auto px-5 pt-5 pb-7 transfrom -translate-y-11">
+              <div className="flex items-center gap-5 text-gray-600">
+                <div className="flex items-center gap-1">
+                  
+                  <img src={advertised.author_info[0]?.autor_image} className=' w-8 h-8 rounded-full' alt="" />
+                  <span>{advertised.author_info[0]?.author_name}</span>
+                </div>
+                <div className="flex items-center gap-1">
+                  <BiCalendar className="text-xl" />
+                  <span>{advertised?.post_date}</span>
+                </div>
+              </div>
+              <h4 className="text-2xl my-3 font-semibold hover:text-secondary transition duration-200">
+                {advertised?.property_heading}
+              </h4>
+              <p>
+                {advertised?.details}
+              </p>
+              <BsArrowRight className="text-2xl font-semibold absolute left-1/2 transform -translate-x-1/2 -bottom-3 bg-secondary/90 transtion duration-300 cursor-pointer hover:bg-secondary w-8 h-8 rounded-full p-1 text-white" />
+            </div>
+          </div>)
+        }
+        
 
-        <div>
-          <div
-            style={{
-              backgroundImage: `linear-gradient(to bottom, rgba(0,0,0,.1), rgba(0,0,0.8)), url(${home4.src})`,
-              backgroundSize: "cover",
-            }}
-            className="w-[375px] h-80 object-cover object-center relative "
-          >
-            {/* Inner Pop Up Div */}
-          </div>
-          <div className="bg-gray-50 w-[330px] mx-auto px-5 pt-5 pb-7 transfrom -translate-y-11">
-            <div className="flex items-center gap-5 text-gray-600">
-              <div className="flex items-center gap-1">
-                <BsPersonFill className="text-xl" />
-                <span>By Admin</span>
-              </div>
-              <div className="flex items-center gap-1">
-                <BiCalendar className="text-xl" />
-                <span>March 9, 2020</span>
-              </div>
-            </div>
-            <h4 className="text-2xl my-3 font-semibold hover:text-secondary transition duration-200">
-              Family House
-            </h4>
-            <p>
-              Lorem ipsum dolor sit amet consectetur adipisicing elit. Facilis
-              aspernatur eligendi sed molestias necessitatibus quia.
-            </p>
-            <BsArrowRight className="text-2xl font-semibold absolute left-1/2 transform -translate-x-1/2 -bottom-3 bg-secondary/90 transtion duration-300 cursor-pointer hover:bg-secondary w-8 h-8 rounded-full p-1 text-white" />
-          </div>
-        </div>
+      
 
-        <div>
-          <div
-            style={{
-              backgroundImage: `linear-gradient(to bottom, rgba(0,0,0,.1), rgba(0,0,0.8)), url(${home4.src})`,
-              backgroundSize: "cover",
-            }}
-            className="w-[375px] h-80 object-cover object-center relative "
-          >
-            {/* Inner Pop Up Div */}
-          </div>
-          <div className="bg-gray-50 w-[330px] mx-auto px-5 pt-5 pb-7 transfrom -translate-y-11">
-            <div className="flex items-center gap-5 text-gray-600">
-              <div className="flex items-center gap-1">
-                <BsPersonFill className="text-xl" />
-                <span>By Admin</span>
-              </div>
-              <div className="flex items-center gap-1">
-                <BiCalendar className="text-xl" />
-                <span>March 9, 2020</span>
-              </div>
-            </div>
-            <h4 className="text-2xl my-3 font-semibold hover:text-secondary transition duration-200">
-              Family House
-            </h4>
-            <p>
-              Lorem ipsum dolor sit amet consectetur adipisicing elit. Facilis
-              aspernatur eligendi sed molestias necessitatibus quia.
-            </p>
-            <BsArrowRight className="text-2xl font-semibold absolute left-1/2 transform -translate-x-1/2 -bottom-3 bg-secondary/90 transtion duration-300 cursor-pointer hover:bg-secondary w-8 h-8 rounded-full p-1 text-white" />
-          </div>
-        </div>
+
       </div>
     </div>
   );
