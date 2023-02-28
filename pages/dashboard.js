5f6d12fc37776367cc1a708f82e6ff64611d9925
SaifullaJubair <saifullajubair6@gmail.com>
2023-02-28 17:54:02 +0600

    dashboard

--- pages/dashboard.js
+++ pages/dashboard.js
@@ -1,11 +1,24 @@
+import React, { useContext } from 'react';
+import Dashboard from '../components/Dashboard/Dashboard/Dashboard';
+import PleaseLogin from '../components/PleaseLogin/PleaseLogin';
+import Loader from '../components/Shared/Loader/Loader';
+import { AuthContext } from '../Contexts/AuthProvider/AuthProvider';
 
-
-const dashboard = () => {
+const DashboardPage = () => {
+   const { user, loading } = useContext(AuthContext)
+   if (loading) {
+      return <Loader></Loader>
+   }
+   if (!user?.email) {
+      return <PleaseLogin></PleaseLogin>
+   }
    return (
+
       <div>
-         this is dashboard
+         <title>DashBoard</title>
+         <Dashboard></Dashboard>
       </div>
    );
 };
 
-export default dashboard;
+export default DashboardPage;
