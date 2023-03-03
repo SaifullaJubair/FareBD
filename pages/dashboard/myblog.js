4f44836f536462d226db5bdfd41e7799071859a3
amitpaultl <amitpaultl@gmail.com>
2023-03-03 11:40:07 +0600

    table add

---
+++ pages/dashboard/myblog.js
@@ -0,0 +1,12 @@
+import MyBlog from "@/Components/Dashboard/MyBlog/MyBlog";
+
+
+const myblog = () => {
+    return (
+        <div>
+            <MyBlog></MyBlog>
+        </div>
+    );
+};
+
+export default myblog;
