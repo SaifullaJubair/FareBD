1143bfcf7ad4b29d1f679fd607db90a52b555fcc
Rezaul <mdrezaulkarrim@gmail.com>
2023-03-06 00:04:33 +0600

    styled properties link

--- Components/Shared/Navbar/Navbar.js
+++ Components/Shared/Navbar/Navbar.js
@@ -58,6 +58,10 @@ const Navbar = () => {
       link: "/",
     },
     {
+      item: "Properties",
+      link: "/properties",
+    },
+    {
       item: "About Us",
       link: "/about",
     },
@@ -175,6 +179,14 @@ const Navbar = () => {
                   aria-current="page"
                 >
                   Home
+                </Link>
+              </li>
+              <li>
+                <Link
+                  href="/properties"
+                  className="block py-2 pl-3 pr-4 text-white rounded hover:bg-gray-100 md:hover:bg-transparent md:hover:text-blue-700 md:p-0 md:dark:hover:text-white dark:text-gray-400 dark:hover:bg-gray-700 dark:hover:text-white md:dark:hover:bg-transparent dark:border-gray-700"
+                >
+                  Properties
                 </Link>
               </li>
               {/* <li>
