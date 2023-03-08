5fc4aa8b1a596948dae4c94c65dcb6c79a3811e3
amitpaultl <amitpaultl@gmail.com>
2023-03-08 22:36:11 +0600

    table add

--- Components/SearchResult/SearchResult.js
+++ Components/SearchResult/SearchResult.js
@@ -11,10 +11,27 @@ import { TbCurrencyTaka } from "react-icons/tb";
 import { TfiLocationPin } from "react-icons/tfi";
 import DivisionSidebar from "@/Components/Home/Division/DivisionSidebar";
 import { AuthContext } from "@/Contexts/AuthProvider/AuthProvider";
+import ReactPaginate from 'react-paginate';
 
 function SearchResult({ searchData }) {
   const { user } = useContext(AuthContext);
   const router = useRouter();
+  const [itemOffset, setItemOffset] = useState(0);
+
+  const itemsPerPage = 4
+
+  const endOffset = itemOffset + itemsPerPage;
+
+  const currentItems = searchData.slice(itemOffset, endOffset);
+  const pageCount = Math.ceil(searchData.length / itemsPerPage);
+
+  const handlePageClick = (event) => {
+    const newOffset = (event.selected * itemsPerPage) % searchData.length;
+    console.log(
+      `User requested page number ${event.selected}, which is offset ${newOffset}`
+    );
+    setItemOffset(newOffset);
+  };
 
   console.log(searchData);
   return (
@@ -30,77 +47,95 @@ function SearchResult({ searchData }) {
               </Link>
             </h1>
           )}
-          {searchData.length > 0 &&
-            searchData?.map((el, i) => (
-              <Link
-                href={`/singleproperty/${el?._id}`}
-                className="flex flex-col items-center bg-white border border-gray-200 shadow-lg md:flex-row hover:bg-gray-100 dark:border-gray-700 dark:bg-gray-800 dark:hover:bg-gray-700 h-72 my-4 mb-8"
-                key={i}
-              >
-                <img
-                  className="h-full w-96"
-                  src={el?.property_picture}
-                  alt="img"
-                />
 
-                <div className="flex flex-col justify-start ml-8 p-4 leading-normal my-4">
-                  <div className="grid gap-1.5">
-                    <p>
-                      <span className="flex font-bold text-xl">
-                        {el?.price}
-                        <TbCurrencyTaka className="mt-1" />
-                      </span>
-                    </p>
-                    {/* <FaGripLinesVertical className='mt-1' /> */}
-                    <p className="font-bold text-start">{el?.property_type}</p>
-                    {/* <FaGripLinesVertical className='mt-1' /> */}
-                    <p>
-                      <span className="flex text-xs">
-                        <TfiLocationPin className="mt-1" /> {el?.location}{" "}
-                      </span>
-                    </p>
+          <div className=" min-h-screen">
+            {currentItems.length > 0 && 
+              currentItems?.map((el, i) => (
+            <Link
+              href={`/singleproperty/${el?._id}`}
+              className="flex flex-col items-center bg-white border border-gray-200 shadow-lg md:flex-row hover:bg-gray-100 dark:border-gray-700 dark:bg-gray-800 dark:hover:bg-gray-700 h-72 my-4 mb-8"
+              key={i}
+            >
+              <img
+                className="h-full w-96"
+                src={el?.property_picture}
+                alt="img"
+              />
+
+              <div className="flex flex-col justify-start ml-8 p-4 leading-normal my-4">
+                <div className="grid gap-1.5">
+                  <p>
+                    <span className="flex font-bold text-xl">
+                      {el?.price}
+                      <TbCurrencyTaka className="mt-1" />
+                    </span>
+                  </p>
+                  {/* <FaGripLinesVertical className='mt-1' /> */}
+                  <p className="font-bold text-start">{el?.property_type}</p>
+                  {/* <FaGripLinesVertical className='mt-1' /> */}
+                  <p>
+                    <span className="flex text-xs">
+                      <TfiLocationPin className="mt-1" /> {el?.location}{" "}
+                    </span>
+                  </p>
+                </div>
+                <h2 className="text-sm mt-4 text-start">
+                  {el?.property_heading}
+                </h2>
+
+                <div className="flex justify-start gap-8 mt-4 font-bold text-secondary">
+                  <div className="flex gap-1">
+                    <MdOutlineApartment className="mt-1" />
+                    <p>{el?.flat_feature[0]?.floor}</p>
                   </div>
-                  <h2 className="text-sm mt-4 text-start">
-                    {el?.property_heading}
-                  </h2>
-
-                  <div className="flex justify-start gap-8 mt-4 font-bold text-secondary">
-                    <div className="flex gap-1">
-                      <MdOutlineApartment className="mt-1" />
-                      <p>{el?.flat_feature[0]?.floor}</p>
-                    </div>
-                    <div className="flex gap-1">
-                      <MdOutlineBedroomChild className="mt-1" />
-                      <p>{el?.flat_feature[0]?.room}</p>
-                    </div>
-                    <div className="flex gap-1">
-                      <MdOutlineBathroom className="mt-1" />
-                      <p>{el?.flat_feature[0]?.bathroom}</p>
-                    </div>
-                    <div className="flex gap-1">
-                      <HiOutlineSquares2X2 className="mt-1" />
-                      <p>{el?.size}</p>
-                    </div>
+                  <div className="flex gap-1">
+                    <MdOutlineBedroomChild className="mt-1" />
+                    <p>{el?.flat_feature[0]?.room}</p>
                   </div>
-
-                  <div className="flex justify-start gap-5 mt-4">
-                    {/* <button className='flex justify-center items-center gap-1 text-white bg-primary px-3 rounded'><HiPhone className='mt-1' />Call</button> */}
-                    <img
-                      className=" h-10 w-10 rounded-full"
-                      src={el?.user_image}
-                      alt=""
-                    />
-                    {/* <button className='flex justify-center items-center gap-1 text-white bg-primary px-3 rounded'><HiEnvelope className='mt-1' />Email</button> */}
-                    <img
-                      className="h-12"
-                      src="https://lh3.googleusercontent.com/pw/AMWts8D7jqd4R67XBB7IKs6Hi8jRKjgJ-2XmxdiU66iGxHdTNdqGNjtsTaPNYu-xcXf7ZOzAvzwtf_zJZzKfA0H7MFaNGFwcuEBsK1nQBXSC6Uxk_lz5eCCKOnf8MsAA0URa3-TL3W-88iNp0tN5eEK94LRq=w538-h274-no?authuser=0"
-                      alt=""
-                    />
+                  <div className="flex gap-1">
+                    <MdOutlineBathroom className="mt-1" />
+                    <p>{el?.flat_feature[0]?.bathroom}</p>
+                  </div>
+                  <div className="flex gap-1">
+                    <HiOutlineSquares2X2 className="mt-1" />
+                    <p>{el?.size}</p>
                   </div>
                 </div>
-              </Link>
-            ))}
+
+                <div className="flex justify-start gap-5 mt-4">
+                  {/* <button className='flex justify-center items-center gap-1 text-white bg-primary px-3 rounded'><HiPhone className='mt-1' />Call</button> */}
+                  <img
+                    className=" h-10 w-10 rounded-full"
+                    src={el?.user_image}
+                    alt=""
+                  />
+                  {/* <button className='flex justify-center items-center gap-1 text-white bg-primary px-3 rounded'><HiEnvelope className='mt-1' />Email</button> */}
+                  <img
+                    className="h-12"
+                    src="https://lh3.googleusercontent.com/pw/AMWts8D7jqd4R67XBB7IKs6Hi8jRKjgJ-2XmxdiU66iGxHdTNdqGNjtsTaPNYu-xcXf7ZOzAvzwtf_zJZzKfA0H7MFaNGFwcuEBsK1nQBXSC6Uxk_lz5eCCKOnf8MsAA0URa3-TL3W-88iNp0tN5eEK94LRq=w538-h274-no?authuser=0"
+                    alt=""
+                  />
+                </div>
+              </div>
+            </Link>
+              ))}
+          </div>
+
+
+          <div className="pagination mt-6">
+            <ReactPaginate
+              breakLabel="..."
+              nextLabel=">"
+              onPageChange={handlePageClick}
+              pageRangeDisplayed={5}
+              pageCount={pageCount}
+              previousLabel="<"
+              renderOnZeroPageCount={null}
+              containerClassName='pagination-menu'
+            />
+          </div>
         </div>
+
 
         <div>
           <DivisionSidebar></DivisionSidebar>
--- pages/searchResult.js
+++ pages/searchResult.js
@@ -1,11 +1,18 @@
 import SearchResult from "@/Components/SearchResult/SearchResult";
-import { useEffect, useState } from "react";
+import { useContext, useEffect, useState } from "react";
 import { useRouter } from "next/router";
+import { AuthContext } from "@/Contexts/AuthProvider/AuthProvider";
+import Loader from "@/Components/Shared/Loader/Loader";
 
 function searchResult() {
+  const { user, loading } = useContext(AuthContext);
   const [searchData, setSearchData] = useState([]);
   const router = useRouter();
   //   const data = router.query;
+    // loading
+  if (loading) {
+    return <Loader></Loader>;
+  }
   useEffect(() => {
     setSearchData(JSON.parse(router.query.data));
   }, [router.query]);
